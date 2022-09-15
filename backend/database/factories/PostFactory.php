cdd4e3dd1401dbe36ba5a257503860c5f28540fc
nawawa <oonawa12@gmail.com>
2022-09-15 22:45:30 +0900

    記事ファクトリー追加

---
+++ backend/database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'slug' => $this->faker->slug(),
+            'title' => $this->faker->text(),
+            'content' => $this->faker->randomHtml(),
+        ];
+    }
+}
